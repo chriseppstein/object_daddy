f250f7d395e4ab4d48edfdaef5a4e2235f2e8b73
Joshua Bates <joshuabates@gmail.com>
2008-07-09 12:13:04 -0700

    Fix plugin install to create exemplars dir in spec if it exists

--- install.rb
+++ install.rb
@@ -1,5 +1,10 @@
 require 'fileutils'
 
-FileUtils.mkdir(File.dirname(__FILE__) + '/../../../test/exemplars') unless File.directory?(File.dirname(__FILE__) + '/../../../test/exemplars')
+%w(test spec).detect do |test_or_spec|
+  dir = File.dirname(__FILE__) + "/../../../#{test_or_spec}"
+  dir = nil unless File.directory?(dir)
+end
+  
+FileUtils.mkdir("#{dir}/exemplars") unless File.directory?("#{dir}/exemplars") if dir
 
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
